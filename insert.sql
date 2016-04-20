00d6315cb6944a911c38788db618e13085d71129
Xuzhe Dang <Xuzhe.Dang@email.asu.edu>
2016-04-20 12:50:34 -0700

    Update insert.sql
    A

--- insert.sql
+++ insert.sql
@@ -1,9 +1,15 @@
+//author = Aakruthi
 insert supplier values 
 (0,'TEST0','TESTF0','TESTL0','123123123','123123@123.com','TESTAddress','TESTCity','TESTState'),
 (1,'TEST1','TESTF1','TESTL1','123123123','123123@123.com','TESTAddress','TESTCity','TESTState'),
 (2,'TEST2','TESTF2','TESTL2','123123123','123123@123.com','TESTAddress','TESTCity','TESTState'),
 (3,'TEST3','TESTF3','TESTL3','123123123','123123@123.com','TESTAddress','TESTCity','TESTState'),
-(4,'TEST4','TESTF4','TESTL4','123123123','123123@123.com','TESTAddress','TESTCity','TESTState');
+(4,'TEST4','TESTF4','TESTL4','123123123','123123@123.com','TESTAddress','TESTCity','TESTState'),
+(5,'TEST5','TESTF5','TESTL5','123123123','123123@123.com','TESTAddress','TESTCity','TESTState'),
+(6,'TEST6','TESTF6','TESTL6','123123123','123123@123.com','TESTAddress','TESTCity','TESTState'),
+(7,'TEST7','TESTF7','TESTL7','123123123','123123@123.com','TESTAddress','TESTCity','TESTState'),
+(8,'TEST8','TESTF8','TESTL8','123123123','123123@123.com','TESTAddress','TESTCity','TESTState'),
+(9,'TEST9','TESTF9','TESTL9','123123123','123123@123.com','TESTAddress','TESTCity','TESTState');
 
 insert Medicine values
 (0,'TEST0',0,'12','120','TESTINFO'),
@@ -14,14 +20,20 @@ insert Medicine values
 (5,'TEST5',3,'12','120','TESTINFO'),
 (6,'TEST6',4,'12','120','TESTINFO'),
 (7,'TEST7',5,'12','120','TESTINFO'),
-(8,'TEST8',6,'12','120','TESTINFO');
+(8,'TEST8',6,'12','120','TESTINFO'),
+(9,'TEST9',6,'12','120','TESTINFO');
 
 insert Department values
 (0,'TEST0'),
 (1,'TEST1'),
 (2,'TEST2'),
 (3,'TEST3'),
-(4,'TEST4');
+(4,'TEST4'),
+(5,'TEST5'),
+(6,'TEST6'),
+(7,'TEST7'),
+(8,'TEST8'),
+(9,'TEST9');
 
 insert Insurance values
 (0,'TEST0',100,'2016/05/01','TESTINFO'),
@@ -29,14 +41,23 @@ insert Insurance values
 (2,'TEST2',300,'2016/05/03','TESTINFO'),
 (3,'TEST3',400,'2016/05/03','TESTINFO'),
 (4,'TEST4',500,'2016/05/04','TESTINFO'),
-(5,'TEST5',600,'2016/05/05','TESTINFO');
+(5,'TEST5',600,'2016/05/05','TESTINFO'),
+(6,'TEST4',500,'2016/05/04','TESTINFO'),
+(7,'TEST4',500,'2016/05/04','TESTINFO'),
+(8,'TEST4',500,'2016/05/04','TESTINFO'),
+(9,'TEST4',500,'2016/05/04','TESTINFO');
 
 insert Bed values
 (0,'1',2,1),
 (1,'1',1,0),
 (2,'2',1,0),
 (3,'2',3,1),
-(4,'3',4,1);
+(4,'3',4,1),
+(5,'2',3,1),
+(6,'2',3,1),
+(7,'2',3,1),
+(8,'2',3,1),
+(9,'2',3,1);
 
 insert Doctor values
 (0,'TESTF0','TESTL0','Doc',30,'M',0,'123123123','TEST0@test',null),
@@ -60,21 +81,31 @@ insert Nurse values
 (6,'TESTF6','TESTL6','MISS',37,'F',2,'123123123','TEST0@test',5),
 (7,'TESTF7','TESTL7','MISS',41,'F',1,'123123123','TEST0@test',null),
 (8,'TESTF8','TESTL8','MISS',29,'M',3,'123123123','TEST0@test',null),
-(9,'TESTF9','TESTL8','MISS',31,'M',4,'123123123','TEST0@test',null);
+(9,'TESTF9','TESTL9','MISS',31,'M',4,'123123123','TEST0@test',null);
 
 insert Patient values
 (0,'TESTF0','TESTL0','M',20,'I','123123123','TEST@test',0,0,0),
 (1,'TESTF1','TESTL1','F',89,'II','123123123','TEST@test',1,1,1),
 (2,'TESTF2','TESTL2','F',34,'III','123123123','TEST@test',1,2,2),
 (3,'TESTF3','TESTL3','M',45,'I','123123123','TEST@test',2,3,3),
-(4,'TESTF4','TESTL4','M',26,'IV','123123123','TEST@test',3,4,4);
+(4,'TESTF4','TESTL4','M',26,'IV','123123123','TEST@test',3,4,4),
+(5,'TESTF3','TESTL3','M',45,'I','123123123','TEST@test',2,3,3),
+(6,'TESTF3','TESTL3','M',45,'I','123123123','TEST@test',2,3,3),
+(7,'TESTF3','TESTL3','M',45,'I','123123123','TEST@test',2,3,3),
+(8,'TESTF3','TESTL3','M',45,'I','123123123','TEST@test',2,3,3),
+(9,'TESTF3','TESTL3','M',45,'I','123123123','TEST@test',2,3,3);
 
 insert Bill values
 (0,0,0,0,null),
 (1,1,344,234,null),
 (2,2,345,100,null),
 (3,3,345,22,null),
-(4,4,7657,0,'2016/04/11');
+(4,4,7657,0,'2016/04/11'),
+(5,5,345,22,'2015/03/02'),
+(6,6,345,22,'2016/04/17'),
+(7,7,345,22,'2015/02/02'),
+(8,8,345,22,'2016/01/01'),
+(9,9,345,22,'2016/01/11');
 
 insert Bill_Detail values
 (0,1,1,'Medicine',100,'2015/12/12'),
@@ -86,5 +117,83 @@ insert Bill_Detail values
 (5,3,1,'Medicine',100,'2015/12/12'),
 (6,3,1,'Medicine',100,'2016/02/12'),
 (7,4,1,'Medicine',100,'2016/03/12'),
-(8,1,1,'Medicine',100,'2016/04/12');
+(8,1,1,'Medicine',100,'2016/04/12'),
+(9,4,1,'Medicine',100,'2016/03/12');
+
+insert into contract(Contract_ID,Supplier_ID,Medince_ID,Quantity,UnitPrice,Invoice,Date)
+values (1,1,1,5,2,10,'2016/01/01'),
+(2,1,2,5,3,15,'2016/02/13'),
+(3,1,3,5,4,20,'2016/03/13'),
+(4,1,4,5,5,25,'2016/04/13'),
+(5,1,5,5,6,30,'2015/05/13'),
+(6,1,6,5,1,5,'2016/06/13'),
+(7,1,7,5,2,10,'2015/07/13'),
+(8,1,8,5,3,15,'2015/08/13'),
+(9,1,9,5,4,20,'2015/01/13'),
+(10,1,1,5,5,25,'2015/01/13');
+
+insert into TreatmentTeam(Patient_ID,Doctor_ID)
+values (1,1),
+(2,2),
+(2,3),
+(3,4),
+(4,5),
+(5,6),
+(6,7),
+(7,8),
+(8,9),
+(1,10);
+
+insert into MedicineUseRecord(MUR_ID,Medince_ID,Doctor_ID,Nurse_ID,Quantity,Patient_ID,UsedDate)
+values (1,1,1,1,5,1,'2016/01/01'),
+(2,2,2,2,5,2,'2016/01/02'),
+(3,3,3,3,5,3,'2016/01/03'),
+(4,4,4,4,5,4,'2016/01/04'),
+(5,5,5,5,5,5,'2016/01/05'),
+(6,6,6,6,5,6,'2016/01/06'),
+(7,7,7,7,5,7,'2016/01/07'),
+(8,8,8,8,5,8,'2016/01/08'),
+(9,9,9,9,5,9,'2016/01/09'),
+(10,10,10,10,5,10,'2016/01/10');
+
+insert into Health_Attribute(Health_ID,Patient_ID,Systolic_BP,Diastolic_BP,BloodSugar,Pulse,Date)
+values (1,2,70,70,80,60,'2016-01-01'),
+(2,3,100,70,80,60,'2016-02-01'),
+(3,4,120,70,80,60,'2016-03-01'),
+(4,5,150,70,80,60,'2016-04-01'),
+(5,6,80,70,80,100,'2016-05-01'),
+(6,7,90,70,80,60,'2016-06-01'),
+(7,8,100,80,80,60,'2016-07-01'),
+(8,9,110,70,80,70,'2016-08-01'),
+(9,10,150,90,80,80,'2016-09-01'),
+(10,1,80,100,80,90,'2016-10-01');
+
+
+
+insert into register(Register8_ID,Name,Doctor_ID,Date)
+values (1,'xyz',1,'2016-01-01'),
+(2,'ABC',2,'2016-01-02'),
+(3,'DEF',3,'2016-01-03'),
+(4,'GHI',4,'2016-01-04'),
+(5,'JKL',5,'2016-01-05'),
+(6,'MNO',6,'2015-01-01'),
+(7,'PQR',7,'2015-01-02'),
+(8,'STU',8,'2015-01-03'),
+(9,'VWX',9,'2015-01-04'),
+(10,'ADA',2,'2015-01-05');
+
+insert into NurseSchedule(Schedule_ID,Nurse_ID,StartTime,EndTime,Bed_ID)
+values (1,1,'6:00:00','15:00:00',1),
+(2,2,'6:00:00','15:00:00',1),
+(3,3,'6:00:00','15:00:00',1),
+(4,4,'6:00:00','15:00:00',1),
+(5,5,'6:00:00','15:00:00',1),
+(6,1,'15:00:00','21:00:00',1),
+(7,2,'6:00:00','21:00:00',1),
+(8,3,'21:00:00','6:00:00',1),
+(9,4,'21:00:00','6:00:00',1),
+(10,5,'21:00:00','6:00:00',1);
+
+
+
 
