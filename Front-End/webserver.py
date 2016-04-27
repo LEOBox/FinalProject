9c772c18f55064ec3a17f9067c246a3a98f61f8f
XimingFeng <xfeng36@asu.edu>
2016-04-26 18:27:13 -0700

    Create Some Basic Page

--- Front-End/webserver.py
+++ Front-End/webserver.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def function():
 	return render_template('index.html')
 
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
+@app.route('/bill')
+def bill():
+	return render_template('bill.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
